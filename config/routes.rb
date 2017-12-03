f89424b4c8e7e194f0ae4ef33e337d7c563af76a
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-12-03 19:41:48 +0200

    posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
+  get 'posts' => 'post#index', as: 'posts_path'
+  get 'pages/index'
+
+  root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
